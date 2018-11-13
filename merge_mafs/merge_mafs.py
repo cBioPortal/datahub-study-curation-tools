ec91c27c2f3b00550a6d3ebd92e8a58caf9c1db2
ramya <ramyamadupuri99@gmail.com>
2018-11-13 11:12:12 -0500

    Added checks to split and merge scripts

--- merge_mafs/merge_mafs.py
+++ merge_mafs/merge_mafs.py
@@ -7,49 +7,43 @@ parser.add_argument("maf_file2", type=str, help="Input MAF2")
 parser.add_argument("merged_maf", type=str, help="Output merged MAF filename")
 args = parser.parse_args()
 
-file_header = list();
-
 # Check if columns in the two input maf files are in same order	
 
-try:
-	with open(args.maf_file1,'r') as file:
-		header = file.readline()
-		file_header.append(header)
-except IOError:
-	print args.maf_file1,"file does not exist."
-	sys.exit()
-	
-try:
-	with open(args.maf_file2,'r') as file:
-		header = file.readline()
-		file_header.append(header)
-except IOError:
-	print args.maf_file2,"file does not exist."
-	sys.exit()
+def get_header(file):
+	try:
+		with open(file, "r") as header_source:
+			for line in header_source:
+				if not line.startswith("#"):
+					header = line
+					break
+		return header
+		
+	except IOError:
+		print args.maf_file1,"file does not exist."
+		sys.exit()
+		
+header1 = get_header(args.maf_file1)
+header2 = get_header(args.maf_file2)
 
-header1 = file_header[0]
-header2 = file_header[1]
 
 if header1 != header2:
 	print("You are trying to concatenate MAF files with columns of differnt order. Exiting..")
 	sys.exit()
 
-# Concatenate multiple MAF's
-
-all_data = str()
+# Concatenate MAF's
 
 with open(args.maf_file1,'r') as file:
-	header = file.readline()
-	for lines in file:
-		all_data += lines
+	data1 = file.read()
 
+data2 = str()
 with open(args.maf_file2,'r') as file:
-	header = file.readline()
-	for lines in file:
-		all_data += lines
+		for line in file:
+			if line.startswith('#') or line.upper().startswith('HUGO_SYMBOL') :
+				continue
+			else:
+				data2 += line
 
 merged_file = open(args.merged_maf,"w")
-merged_file.write(header1)
-merged_file.write(all_data)
+merged_file.write(data1)
+merged_file.write(data2)
 merged_file.close()
-
--- split_maf/README.md
+++ split_maf/README.md
@@ -1,7 +1,7 @@
-### Usage
+#### Usage
 Split Genome Nexus annotated ```data_mutations.txt``` to ```annotated_mutations.txt``` and ```unannotated_muatations.txt``` tables based on the HGVSp_Short column.
 
-### Command Line
+#### Command Line
 ```
 python split_maf.py path/to/data_mutations.txt path/to/annotated_mutations.txt path/to/unannotated_mutations.txt
 ```
