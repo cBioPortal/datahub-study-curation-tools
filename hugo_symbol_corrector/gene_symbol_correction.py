e1b2c374d340f266ab2483b88ceb215ede056988
ramya <ramyamadupuri99@gmail.com>
2019-01-15 11:19:25 -0500

    Update the script to print modified gene symbols'

--- hugo_symbol_corrector/gene_symbol_correction.py
+++ hugo_symbol_corrector/gene_symbol_correction.py
@@ -7,24 +7,36 @@ with open(sys.argv[1],"r") as mp:
 		line = line.rstrip('\n')
 		genes = line.split("\t")
 		gene_dict[genes[1]] = genes[0]
-
-mp.close()
-data = str()
+	
+comments = ""
+header = ""
+data = ""
+replaced_list = {}
 
 with open(sys.argv[2],"r") as file:
-	header = file.readline()
 	for line in file:
-		values = line.split("\t")
-		values[0] = values[0].strip('\t')
-		if values[0].upper() in gene_dict:
-			line = line.replace(values[0],gene_dict[values[0].upper()])
-			data += line
+		if line.startswith('#'):
+			comments += line
+		elif line.startswith('Hugo_Symbol'):
+			header += line
 		else:
-			data += line
+			values = line.split("\t")
+			values[0] = values[0].strip('\t')
+			if values[0].upper() in gene_dict:
+				line = line.replace(values[0],gene_dict[values[0].upper()],1)
+				replaced_list[values[0]] = gene_dict[values[0].upper()]
+				data += line
+			else:
+				data += line	
 
-file.close()
 os.remove(sys.argv[2])
 
 new_file = open(sys.argv[2],"w")
+new_file.write(comments)
 new_file.write(header)
 new_file.write(data)
+new_file.close()
+
+print("Corrected Gene Symbols:")
+for val in replaced_list:
+	print(val+"  ->  "+replaced_list[val])
