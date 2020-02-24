6412fe9edc950016704b3b0e9e9026db91906733
ramya <ramyamadupuri99@gmail.com>
2020-02-24 17:01:02 -0500

    updated script and readme

--- generate_Zscores/NormalizeExpressionLevels.py
+++ generate_Zscores/NormalizeExpressionLevels.py
@@ -1,22 +1,54 @@
-import sys
-import os
-import statistics
-import argparse
-import math
+#! /usr/bin/env python
 
-# Given expression data for a set of samples generate normalized expression values (Independant of unaltered status from CNA file)
 #
-# Each gene is normalized separately. First, the expression distribution of the gene is
-# estimated by calculating the mean and variance of the expression values for samples whose
-# values are not Null, NA or NaN
+# Copyright (c) 2018 Memorial Sloan Kettering Cancer Center.
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+# documentation provided hereunder is on an "as is" basis, and
+# Memorial Sloan Kettering Cancer Center
+# has no obligations to provide maintenance, support,
+# updates, enhancements or modifications.  In no event shall
+# Memorial Sloan Kettering Cancer Center
+# be liable to any party for direct, indirect, special,
+# incidental or consequential damages, including lost profits, arising
+# out of the use of this software and its documentation, even if
+# Memorial Sloan Kettering Cancer Center
+# has been advised of the possibility of such damage.
 #
-# If the gene has samples whose expression values are Null, NaN or 'NA', then its normalized expression is reported as NA.
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# ---------------------------------------------------------------------------------
+# Given the expression data for a set of samples, this script generates normalized
+# expression values with the reference population of all samples and independent of
+# sample diploid status.
+
+# METHOD:
+# Each gene is normalized separately. First, the expression distribution of the gene
+# is estimated by calculating the mean and variance of the expression values for
+# samples whose values are not Null, NA or NaN
+#
+# If the gene has samples whose expression values are Null, NaN or 'NA', then its
+# normalized expression is reported as NA.
 # Otherwise, for every sample, the gene's normalized expression is reported as
 #
 # (r - mu)/sigma
 #
-# where r is the raw expression value, and mu and sigma are the mean and standard deviation
-# of the samples with expression values, respectively.
+# where r is the raw expression value, and mu and sigma are the mean and standard
+# deviation of the samples with expression values, respectively.
 #
 # The syntax is simple:
 #
@@ -30,15 +62,30 @@ import math
 #
 # Algorithm:
 # Input expression file
-# concept:
 # for each gene:
-# 		compute mean and standard deviation for samples ( n = # of samples where expression value is not Null, NA, NaN)
+#		compute mean and standard deviation for samples ( n = # of samples where expression value is not Zero, Null, NA, NaN)
 #		for each sample:
 #		compute zScore when standard deviation != 0
 #		output NA for genes with standard deviation = 0
 #
+# We require the mRNA expression data to be log transformed before calculating zscores on them.
+# If the data is not already log tranformed use the -l option to calculate log base 2 of the expression values.
+#
+# Here's how we handle the Negative values:
+#
+# If the value(x) is <= 0 replace the value to 0 and then calculate the log of all values as by adding +1 as log(x+1)
+# example, if raw value is -1, the log transform would be log(0+1)
+#    	   if the value is 0, the log transform would be log(0+1)
+#          if the value is 1, the log transform would be log(1+1)
+# ---------------------------------------------------------------------------------
 
-HEADER_KEYWORDS = ['Composite.Element.REF', 'Hugo_Symbol', 'Entrez_Gene_Id']
+import sys
+import os
+import argparse
+import math
+
+
+HEADER_KEYWORDS = ['Composite.Element.REF','Hugo_Symbol','Entrez_Gene_Id']
 
 # Function to calculate Mean
 def calculate_mean(data,n):
@@ -63,12 +110,13 @@ def calculate_mean_std(line,start_position):
 	expression_data = line.rstrip('\n').rstrip('\r').split('\t')
 	expression_values = expression_data[start_position:]
 
-	# Remove strings ('' or 'NA' or 'NaN') and zeros from the expression list
+	# Remove strings ('' or 'NA' or 'NaN') and Zeros from the expression list
 	filtered_expression_values = []
 	for item in expression_values:
 		try:
 			val = float(item)
-			filtered_expression_values.append(val)
+			if val != 0:
+				filtered_expression_values.append(val)
 		except:
 			continue
 
@@ -84,8 +132,18 @@ def calculate_mean_std(line,start_position):
 		#sigma = statistics.stdev(data,mu)
 	return(mu,sigma)
 
+#If standard deviation is 0.0? print NA as the normalized z-score
+def zero_std(line,start_position):
+	exp_values = line.split('\t')
+	z_scores = exp_values[:start_position]
+	exp_values = exp_values[start_position:]
+	for value in exp_values:
+		z_scores.append('NA')
+	normalised_scores = '\t'.join(z_scores)
+	return(normalised_scores)
+
 # Calculate z_scores
-def calculate_z_scores(line,mu,sigma,start_position):
+def zscores_eachrow(line,mu,sigma,start_position):
 	exp_values = line.split('\t')
 	output_list = exp_values[:start_position]
 	exp_values = exp_values[start_position:]
@@ -102,32 +160,6 @@ def calculate_z_scores(line,mu,sigma,start_position):
 
 	normalized_exp_values = '\t'.join(output_list)
 	return(normalized_exp_values)
-
-#If standard deviation is 0.0? print NA as the normalized z-score
-def zero_std(line,start_position):
-	exp_values = line.split('\t')
-	z_scores = exp_values[:start_position]
-	exp_values = exp_values[start_position:]
-	for value in exp_values:
-		z_scores.append('NA')
-	normalised_scores = '\t'.join(z_scores)
-	return(normalised_scores)
-
-#Check if the file has normal samples
-def check_normals(header):
-	normals_suffix = ['10','11','12','13','14']
-	cols = header.rstrip('\n').split('\t')
-	normal_sample_list = []
-	for x in cols:
-		ar = x.split('-')
-		if ar[len(ar)-1] in normals_suffix:
-			normal_sample_list.append(x)
-	if len(normal_sample_list) != 0:
-		print('ERROR: The file contains the following normal samples')
-		for sample in normal_sample_list:
-			print(sample)
-		print('Remove normals before calculating zscores. Exiting..')
-		sys.exit(2)
 
 # Check the file type and sample start_position:
 def find_sample_position(infile):
@@ -157,46 +189,92 @@ def find_sample_position(infile):
 		sys.exit(2)
 	return(sample_position)
 
+def log_transform_data(input_filename,sample_position):
+	log_transformed_data = ""
+	with open(input_filename, 'r') as infile:
+		print("log transforming data..")
+		for line in infile:
+			if line.startswith('#'):
+				log_transformed_data += line
+			elif line.startswith('Composite.Element.REF') or line.startswith('Hugo_Symbol') or line.startswith('Entrez_Gene_Id'):
+				log_transformed_data += line
+			else:
+				cols = line.rstrip('\n').rstrip('\r').split('\t')
+				scores = cols[sample_position:]
+				transformed_cols = []
+				transformed_cols.extend(cols[:sample_position])
+
+				for val in scores:
+					try:
+						val = float(val)
+						if val<=0:
+							val = math.log(1,2)
+							transformed_cols.append(val)
+						else:
+							val = math.log(val+1,2)
+							transformed_cols.append(val)
+					except:
+						transformed_cols.append(val)
+				data_line = '\t'.join(map(str,transformed_cols))
+				log_transformed_data += data_line+'\n'
+
+	log_transformed_data = log_transformed_data.rstrip('\n')
+	log_transformed_list = log_transformed_data.split('\n')
+	return(log_transformed_list)
+
+def z_scores(data_list,sample_position):
+	zscores_data = ""
+	for line_count,line in enumerate(data_list):
+		line = line.rstrip('\n')
+		if line.startswith('#'):
+			zscores_data =  zscores_data + line + '\n'
+		elif line.startswith('Composite.Element.REF') or line.startswith('Hugo_Symbol') or line.startswith('Entrez_Gene_Id'):
+			zscores_data =  zscores_data + line + '\n'
+		else:
+			mu, sigma = calculate_mean_std(line, sample_position)
+			# If standard deviation == 0 print NA as normalized values
+			if sigma == 0:
+				scores_std = zero_std(line,sample_position)
+				zscores_data += scores_std+'\n'
+			else:
+				sys.stdout.write("\rCalculating zscores on row : %s" % str(line_count))
+				normalised_scores = zscores_eachrow(line,mu,sigma,sample_position)
+				zscores_data += normalised_scores+'\n'
+	return(zscores_data)
+
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-i', '--input_file', required = True, help = 'Expression file for which z-score has to be computed',type = str)
-	parser.add_argument('-o', '--output_file', required = True, help = 'Output file name',type = str)
+	parser.add_argument('-i','--input-expression-file', action = 'store', dest = 'input_expression_file', required = True, help = 'The expression filename to normalize')
+	parser.add_argument('-o','--output-filename', action = 'store', dest = 'output_filename', required = True, help = 'The filename to which normalized data has to be saved')
+	parser.add_argument('-l','--log-transform', action = 'store_true', dest = 'log_transform', required = False, help = 'Pass this argument to log transform the data before calculating zscores')
 	args = parser.parse_args()
 
+	input_filename = args.input_expression_file
+	output_filename = args.output_filename
+	log_transform = args.log_transform
+
 	# check if the input file exists
-	if not os.path.isfile(args.input_file):
-		print("ERROR: The file %s doesn't exist or is not a file" % (args.input_file))
+	if not os.path.isfile(input_filename):
+		print("ERROR: The file %s doesn't exist or is not a file" % (input_filename))
 		parser.print_help()
 		sys.exit(2)
 
 	# check if the file format is correct and get the sample position
-	sample_position = find_sample_position(args.input_file)
-
-	zscores_data = ""
-	with open(args.input_file,'r') as exp_file:
-		for line_count, line in enumerate(exp_file):
-			line = line.rstrip('\n')
-			if line.startswith('#'):
-				zscores_data =  zscores_data + line + '\n'
-			elif line.startswith('Composite.Element.REF') or line.startswith('Hugo_Symbol') or line.startswith('Entrez_Gene_Id'):
-				zscores_data =  zscores_data + line + '\n'
-				#Check if the file has normal samples (Applies to only TCGA studies)
-				#check_normals(line) # Wroks for only TCGA studies.
-			else:
-				mu, sigma = calculate_mean_std(line, sample_position)
-				# If standard deviation == 0 print NA as normalized values
-				if sigma == 0:
-					scores_std = zero_std(line,sample_position)
-					zscores_data += scores_std+'\n'
-				else:
-					sys.stdout.write("\rNormalizing row : %s" % str(line_count))
-					normalised_scores = calculate_z_scores(line,mu,sigma,sample_position)
-					zscores_data += normalised_scores+'\n'
-
-	outfile = open(args.output_file,'w')
+	sample_position = find_sample_position(input_filename)
+	
+	# If -l is passed, log transform the data before calculating zscores
+	if log_transform:
+		log_transformed_list = log_transform_data(input_filename,sample_position)
+		zscores_data = z_scores(log_transformed_list,sample_position)
+	else:
+		data_list = [line.rstrip('\n') for line in open(input_filename)]
+		zscores_data = z_scores(data_list,sample_position)
+	
+	outfile = open(output_filename,'w')
+	sys.stdout.write("\nWriting to file..")
 	outfile.write(zscores_data.rstrip('\n'))
 	outfile.close()
-	print("\nDONE!\n")
+	print("\nDONE!")
 
 if __name__ == '__main__':
 	main()
