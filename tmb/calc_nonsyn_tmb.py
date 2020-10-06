51d9064abdfcfa4eb889e94f6d3c8925ab4a4638
Yichao Sun <yichao.sunny@gmail.com>
2020-10-06 15:34:14 -0400

    revert head by 1

--- tmb/calc_nonsyn_tmb.py
+++ tmb/calc_nonsyn_tmb.py
@@ -37,20 +37,10 @@ import os
 import argparse
 import math
 
-CLIN_ID_COL_ID = "SAMPLE_ID"
+# WGS/WES CDS
+WES_WGS_CDS = 30
 
-CLIN_TMB_COL_ID = "TMB_nonsynonymous"
-CLIN_TMB_COL_PRIORITY = "1"
-CLIN_TMB_COL_TYPE = "STRING"
-CLIN_TMB_COL_DES = "TMB (nonsynonymous)"
-CLIN_TMB_COL_NAME = "TMB (nonsynonymous)"
-
-PANEL_FIELD_NAME = "number_of_profiled_coding_base_pairs:"
-
-MAF_VC_COL_ID = "Variant_Classification"
-MAF_ID_COL_ID = "Tumor_Sample_Barcode"
-MAF_SOMATIC_COL_ID = "Mutation_Status"
-
+# egilible variants (for counting mutations)
 VC_NONSYNONYMOUS_LIST = [
 	"Frame_Shift_Del", 
 	"Frame_Shift_Ins", 
@@ -63,21 +53,85 @@ VC_NONSYNONYMOUS_LIST = [
 	"Splice_Region"
 ]
 
-def extractCDS(_panel):
+# names - MAF file
+MAF_FILE_NAME = "data_mutations_extended.txt"
+MAF_VC_COL_ID = "Variant_Classification"
+MAF_ID_COL_ID = "Tumor_Sample_Barcode"
+MAF_SOMATIC_COL_ID = "Mutation_Status"
 
-	_cdsLength = 0
-	with open(_panel,'r') as _panel:
-		for _line in _panel:
-			if _line.startswith('number_of_profiled_coding_base_pairs:'):
-				_cdsLength = _line.split(':')[1].strip()
+# names  - matrix file
+MATRIX_FILE_NAME = "data_gene_matrix.txt"
+MATRIX_SAMPLE_ID_COL = "SAMPLE_ID"
+MATRIX_MUT_COL = "mutations"
 
-	return float(int(_cdsLength) / 1000000)
+# names - clinical file
+CLIN_INPUT_FILE_NAME = "data_clinical_sample.txt"
+CLIN_OUTPUT_FILE_NAME = "tmb_output_data_clinical_sample.txt"
+CLIN_ID_COL_ID = "SAMPLE_ID"
+CLIN_TMB_COL_ID = "TMB_NONSYNONYMOUS"
+CLIN_TMB_COL_PRIORITY = "1"
+CLIN_TMB_COL_TYPE = "STRING"
+CLIN_TMB_COL_DES = "TMB (nonsynonymous)"
+CLIN_TMB_COL_NAME = "TMB (nonsynonymous)"
 
-def cntVariants(_maf):
+# names - panel file
+PANEL_STABLE_ID_FIELD_NAME = "stable_id:"
+PANEL_CDS_FIELD_NAME = "number_of_profiled_coding_base_pairs:"
+
+
+###### 
+# extract coding sequence length from gene panels and map to each sample
+######
+def extractCDS(_inputStudyFolder, _panelFolderPath, _sampleMap):
+
+	_cdsMap = {}
+
+	# extract CDS field from all panel files
+	for _panelFileName in os.listdir(_panelFolderPath):
+		with open(_panelFolderPath + "/" + _panelFileName ,'r') as _panelFile:
+			_panelID = ""
+			for _line in _panelFile:
+				if _line.startswith(PANEL_STABLE_ID_FIELD_NAME):
+					_panelID = _line.split(':')[1].strip()
+					_cdsMap[_panelID] = float(0)
+				elif _line.startswith(PANEL_CDS_FIELD_NAME):
+					_cdsMap[_panelID] = float(int(_line.split(':')[1].strip()) / 1000000)
+
+	# using gene matrix to map CDS to each sample 
+	with open(_inputStudyFolder + "/" + MATRIX_FILE_NAME ,'r') as _matrixFile:
+		_posSampleId = -1
+		_posMutPanel = -1
+		for _line in _matrixFile:
+			if _line.startswith(MATRIX_SAMPLE_ID_COL):
+				# parsing matrix header
+				_headers = _line.rstrip("\n").rstrip('\r').split('\t')
+				_posSampleId = _headers.index(MATRIX_SAMPLE_ID_COL)
+				_posMutPanel = _headers.index(MATRIX_MUT_COL)
+			else:
+				# parsing content
+				_items = _line.rstrip("\n").rstrip('\r').split('\t')
+				_sampleID = _items[_posSampleId]
+				_panelID = _items[_posMutPanel]
+				_cds = _cdsMap[_panelID]
+				if _sampleMap.has_key(_sampleID):
+					_sampleMap[_sampleID]["cds"] = _cds
+				else:
+					# sample in matrix (sequenced) but not in MAF
+					_sampleMap[_sampleID] = { 
+						'vc_count': 0,
+						'tmb': 0,
+						'cds': _cds
+					}
+	
+	return _sampleMap
+
+###### 
+# count eligible variants for all samples from MAF
+######
+def cntVariants(_inputStudyPath):
 
 	_result = {}
-
-	with open(_maf,'r') as _maf:
+	with open(_inputStudyPath + "/" + MAF_FILE_NAME,'r') as _maf:
 
 		_headers = []
 		_posSampleID = -1
@@ -104,19 +158,26 @@ def cntVariants(_maf):
 					else:
 						_result[_sampleID] = {
 							'vc_count': 1,
-							'tmb': 0
+							'tmb': 0,
+							'cds': WES_WGS_CDS # samples not in matrix are WGS/WES
 						}
 
 	return _result
 
-def calcTMB(_sampleDict, _cdsLength):
+###### 
+# calculate TMB for every sample 
+######
+def calcTMB(_sampleMap):
 
-	for _sampleID in _sampleDict:
-		_sampleDict[_sampleID]["tmb"] = _sampleDict[_sampleID]["vc_count"] / _cdsLength
+	for _sampleID in _sampleMap:
+		_sampleMap[_sampleID]["tmb"] = _sampleMap[_sampleID]["vc_count"] / _sampleMap[_sampleID]["cds"]
 
-	return _sampleDict
+	return _sampleMap
 
-def addTMB(_inputClinFile, _outputClinFile, _sampleTmbMap):
+###### 
+# add TMB column to clinical file
+######
+def addTMB(_inputStudyFolder, _sampleTmbMap):
 
 	_headerItems = [	
 		CLIN_TMB_COL_ID,	
@@ -127,9 +188,9 @@ def addTMB(_inputClinFile, _outputClinFile, _sampleTmbMap):
 	]
 
 	_posSampleID = -1
-	_outputClinFile = open(_outputClinFile,'w')
+	_outputClinFile = open(_inputStudyFolder + "/" + CLIN_OUTPUT_FILE_NAME,'w')
 
-	with open(_inputClinFile,'r') as _inputClinFile:
+	with open(_inputStudyFolder + "/" + CLIN_INPUT_FILE_NAME,'r') as _inputClinFile:
 
 		for _line in _inputClinFile:
 			
@@ -145,7 +206,10 @@ def addTMB(_inputClinFile, _outputClinFile, _sampleTmbMap):
 			# values
 			else: 
 				_sampleID = _line.split("\t")[_posSampleID]
-				_newline = _line.rstrip("\n") + "\t" + str(_sampleTmbMap[_sampleID]["tmb"])
+				if _sampleTmbMap.has_key(_sampleID):
+					_newline = _line.rstrip("\n") + "\t" + str(_sampleTmbMap[_sampleID]["tmb"])
+				else:
+					_newline = _line.rstrip("\n") + "\t" + "NA"
 
 			_outputClinFile.write(_newline + "\n")
 
@@ -153,38 +217,15 @@ def addTMB(_inputClinFile, _outputClinFile, _sampleTmbMap):
 def main():
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-p','--input-gene-panel-filename', action = 'store', dest = 'input_gene_panel_filename', required = False, help = 'Gene panel filename')	
-	parser.add_argument('-m','--input-maf-filename', action = 'store', dest = 'input_maf_filename', required = True, help = 'MAF filename')
-	parser.add_argument('-c','--input-clinical-filename', action = 'store', dest = 'input_clinical_filename', required = True, help = 'Clinical (sample) filename')
-	parser.add_argument('-o','--output-clinical-file-name', action = 'store', dest = 'output_clinical_filename', required = True, help = 'Name of the output clinical file')
+	parser.add_argument('-i','--input-study-folder', action = 'store', dest = 'input_study_folder', required = False, help = 'Input Study folder')	
+	parser.add_argument('-p','--input-gene-panel-folder', action = 'store', dest = 'input_gene_panel_folder', required = False, help = 'Gene Panel folder')	
 	args = parser.parse_args()
 
-	maf_filename = args.input_maf_filename
-	gene_panel_filename = args.input_gene_panel_filename
-	input_clinical_filename = args.input_clinical_filename
-	output_clinical_filename = args.output_clinical_filename
+	_genePanelFolder = args.input_gene_panel_folder
+	_inputStudyFolder = args.input_study_folder
 
-
-	# check if the input file exists
-	if not os.path.isfile(maf_filename):
-		print("ERROR: MAF file %s doesn't exist or is not a file" % (maf_filename))
-		parser.print_help()
-		sys.exit(2)
-
-	if not os.path.isfile(gene_panel_filename):
-		print("ERROR: Gene Panel file %s doesn't exist or is not a file" % (gene_panel_filename))
-		parser.print_help()
-		sys.exit(2)
-
-	if not os.path.isfile(input_clinical_filename):
-		print("ERROR: Clinical file %s doesn't exist or is not a file" % (clinical_filename))
-		parser.print_helpinput_clinical_filename		
-		sys.exit(2)
-
-	# calculation
-	# output --- Dictionary {ID1: {mut #:, tmb:}, ID2: {mut #:, tmb:}, ...}
-	_sampleTmbMap = calcTMB(cntVariants(maf_filename), extractCDS(gene_panel_filename))
-	addTMB(input_clinical_filename, output_clinical_filename, _sampleTmbMap)
+	_sampleTmbMap = calcTMB(extractCDS(_inputStudyFolder, _genePanelFolder, cntVariants(_inputStudyFolder)))
+	addTMB(_inputStudyFolder, _sampleTmbMap)
 
 if __name__ == '__main__':
 	main()
