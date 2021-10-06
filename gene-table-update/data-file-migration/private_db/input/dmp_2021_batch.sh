eec252f7b24d6c2e4614b0cb0753f4dd97c71b62
Yichao Sun <yichao.sunny@gmail.com>
2021-10-06 01:55:43 -0400

    expand outdated gene lists based on private DB mutation investigation

---
+++ gene-table-update/data-file-migration/private_db/input/dmp_2021_batch.sh
@@ -0,0 +1,60 @@
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact/data_CNA.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact/data_SV.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact/data_fusions.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact/data_fusions_gml.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact/data_mutations_extended.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact/data_mutations_manual.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact/data_mutations_nonsignedout.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact/mskimpact_data_cna_hg19.seg
+
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mixedpact/data_CNA.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mixedpact/data_SV.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mixedpact/data_fusions.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mixedpact/data_fusions_gml.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mixedpact/data_mutations_extended.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mixedpact/data_mutations_manual.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mixedpact/data_mutations_nonsignedout.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mixedpact/mixedpact_data_cna_hg19.seg
+
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/sclc_mskimpact_2017/data_CNA.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/sclc_mskimpact_2017/data_fusions.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/sclc_mskimpact_2017/data_mutations_extended.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/sclc_mskimpact_2017/sclc_mskimpact_2017_data_cna_hg19.seg
+
+
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskraindance/data_mutations_extended.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskraindance/data_mutations_nonsignedout.txt
+
+
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/msk_solid_heme/data_CNA.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/msk_solid_heme/data_SV.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/msk_solid_heme/data_fusions.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/msk_solid_heme/data_fusions_gml.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/msk_solid_heme/data_mutations_extended.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/msk_solid_heme/data_mutations_manual.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/msk_solid_heme/data_mutations_nonsignedout.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/msk_solid_heme/mskimpact_data_cna_hg19.seg
+
+
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskaccess/data_CNA.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskaccess/data_SV.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskaccess/data_fusions.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskaccess/data_mutations_extended.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskaccess/data_mutations_nonsignedout.txt
+
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskarcher_unfiltered/data_SV.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskarcher_unfiltered/data_fusions.txt
+
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/lymphoma_super_cohort_fmi_msk/data_CNA.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/lymphoma_super_cohort_fmi_msk/data_SV.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/lymphoma_super_cohort_fmi_msk/data_fusions.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/lymphoma_super_cohort_fmi_msk/data_mutations_annotated.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/lymphoma_super_cohort_fmi_msk/data_mutations_extended.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/lymphoma_super_cohort_fmi_msk/data_mutations_nonsignedout.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/lymphoma_super_cohort_fmi_msk/lymphoma_super_cohort_fmi_msk_data_cna_hg19.seg
+
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact_ped/data_CNA.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact_ped/data_fusions.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact_ped/data_fusions_gml.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact_ped/data_mutations_extended.txt
+python3 gene_cleanup_data_file_migration.py -path /Users/suny1/github/dmp-2021/mskimpact_ped/mskimpact_ped_data_cna_hg19.seg
