582a708300cc9b33284410cdcd706173dff5a4b9
rmadupuri <ramyamadupuri99@gmail.com>
2022-04-01 10:20:26 -0400

    update readme

--- fusion-to-sv-converter/README.md
+++ fusion-to-sv-converter/README.md
@@ -1,19 +1,18 @@
 ### Usage
 
-This tool is intended to convert the Fusion files to the Structural variant format.
+This tool is intended to convert the Fusion files to the Structural Variant data format.
 
 The source fusion file must be in cBioPortal's standard [fusion file format](https://docs.cbioportal.org/5.1-data-loading/data-loading/file-formats#fusion-data) with at least a minimum of `Hugo_Symbol`, `Entrez_Gene_Id`, `Tumor_Sample_Barcode` and `Fusion` fields.
-The tool outputs the fusion data in the cBioPortals [SV file format](https://docs.cbioportal.org/5.1-data-loading/data-loading/file-formats#structural-variant-data)
+
+The tool outputs the fusion data in the cBioPortals [SV file format](https://docs.cbioportal.org/5.1-data-loading/data-loading/file-formats#structural-variant-data).
 
 #### Running the tool
 
 The tool can be run with the following command:
 
 ```
-python3 fusion_to_sv_converter.py [-h] --fusion_file <path/to/data_fusions.txt> --sv_file <path/to/data_sv.txt> --source_repo repo/name
+python3 fusion_to_sv_converter.py [-h] --fusion_file <path/to/data_fusions.txt> --sv_file <path/to/data_sv.txt>
 ```
-
-If the `--source_repo` option is not passed, the repo-name is defaulted to `cmo-argos`.
 
 #### Command Line
 ```
@@ -29,6 +28,7 @@ optional arguments:
                         Path to the fusion file
   -s SV_FILE, --sv_file SV_FILE
                         Path to save the structural variant file
-  -repo [SOURCE_REPO], --source_repo [SOURCE_REPO]
-                        Defaulted to cmo-argos. Enter the source repo of the fusion file otherwise.
 ```
+
+####NOTE:
+- In case of duplicate entries (records that differ by values other than in the `Hugo_Symbol`, `Entrez_Gene_Id`, `Tumor_Sample_Barcode` and `Fusion` fields), the script picks the last occuring value.
--- fusion-to-sv-converter/fusion_to_sv_converter.py
+++ fusion-to-sv-converter/fusion_to_sv_converter.py
@@ -7,8 +7,7 @@ from operator import itemgetter
 
 def write_data(final_mapped_data, header, outfile):
 	updated_header = ['Sample_ID', 'Site1_Hugo_Symbol', 'Site1_Entrez_Gene_Id', 'Site2_Hugo_Symbol', 'Site2_Entrez_Gene_Id', 'SV_Status']
-	additional_columns = [x for x in header if x not in updated_header]
-	additional_columns = [x for x in additional_columns if x not in ['index', 'Hugo_Symbol', 'Entrez_Gene_Id', 'Tumor_Sample_Barcode']]
+	additional_columns = [x for x in header if x not in updated_header and x not in ['index', 'Hugo_Symbol', 'Entrez_Gene_Id', 'Tumor_Sample_Barcode']]
 	updated_header.extend(additional_columns)
 
 	with open(outfile, 'w') as sv_data:
@@ -20,7 +19,7 @@ def map_fusion_to_sv(fusion_dict):
 	final_mapped_data = list()
 	
 	for id in fusion_dict:
-		fusion_column_value = fusion_dict[id][0]['Fusion']
+		fusion_column_value = fusion_dict[id][0]['Event_Info']
 		
 		#Find the fusion partner1 and partner2 genes based on their position in Fusion column. Add the index field.
 		for row in range(len(fusion_dict[id])):
@@ -30,10 +29,13 @@ def map_fusion_to_sv(fusion_dict):
 			
 		#Sort by the index values
 		fusion_dict[id].sort(key= lambda x: (x['index'] is not None, x['index']), reverse=False)
+		print(fusion_dict[id])
+		print('\n')
 		
-		#Group by index values. (Picks the first occurence if duplicated)
+		#Group by index values. (Picks the last occurence if duplicated)
 		dict1 = {}
 		for key, value in groupby(fusion_dict[id], key = itemgetter('index')):
+			print(key, dict(j for i in value for j in i.items()))
 			if key != None:
 				new_dict = dict(j for i in value for j in i.items())
 				if len(dict1) == 0:
@@ -67,28 +69,30 @@ def file_format_check(header, infile):
 	min_required_columns = ['Tumor_Sample_Barcode', 'Fusion', 'Hugo_Symbol', 'Entrez_Gene_Id']
 	for element in min_required_columns:
 		if element not in header:
-			print("The input fusion file: "+infile+" format is invalid. Please refer to https://docs.cbioportal.org/5.1-data-loading/data-loading/file-formats#fusion-data for the allowed fields.")
+			print("The input fusion file: "+infile+" format is invalid. Please refer to https://docs.cbioportal.org/5.1-data-loading/data-loading/file-formats#fusion-data for the allowed data fields.")
 			sys.exit(1)
 
 def interface():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-f', '--fusion_file', required = True, help = 'Path to the fusion file',type = str)
 	parser.add_argument('-s', '--sv_file', required = True, help = 'Path to save the structural variant file',type = str)
-	parser.add_argument('-repo', '--source_repo', nargs='?', const='cmo-argos', type=str, default='cmo-argos', help = 'Defaulted to cmo-argos. Enter the source repo of the fusion file otherwise.')
 	args = parser.parse_args()
 	return args
 	
 def main(parsed_args):
 	
 	data = csv.DictReader(open(parsed_args.fusion_file, 'r'), delimiter='\t')
-	header = data.fieldnames
-	
+
 	#Check the input file format - the file should have at least the following fields - Tumor_Sample_Barcode, Fusion, Hugo_Symbol, Entrez_Gene_Id
-	file_format_check(header, parsed_args.fusion_file)
+	file_format_check(data.fieldnames, parsed_args.fusion_file)
 	
+	#Update fieldnames by fusion source type. Connection_type is stored in frame field for cmo-argos. 
+	data.fieldnames = ['Event_Info' if item == 'Fusion' else item for item in data.fieldnames]
+	data.fieldnames = ['Site2_Effect_On_Frame' if item == 'Frame' else item for item in data.fieldnames]
+
 	fusion_dict = {}
 	for row in data:
-		key_val = row['Tumor_Sample_Barcode']+'_'+row['Fusion']
+		key_val = row['Tumor_Sample_Barcode']+'_'+row['Event_Info']
 		if key_val not in fusion_dict:
 			fusion_dict[key_val] = list()
 			fusion_dict[key_val].append(row)
@@ -96,14 +100,8 @@ def main(parsed_args):
 			fusion_dict[key_val].append(row)
 
 	sv_data = map_fusion_to_sv(fusion_dict)
-	
-	if parsed_args.source_repo == 'cmo-argos':
-		if 'Frame' in header: header = ['Connection_Type' if item == 'Frame' else item for item in header]
-		for val in sv_data:
-			if 'Frame' in val:
-				val['Connection_Type'] = val.pop('Frame')
 
-	write_data(sv_data, header, parsed_args.sv_file)
+	write_data(sv_data, data.fieldnames, parsed_args.sv_file)
  
 if __name__ == '__main__':
 	parsed_args = interface()
